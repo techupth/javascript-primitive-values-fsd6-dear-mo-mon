7dd296b6364abb80dab8f3269d46a5e04f70c388
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-05-09 14:51:56 +0700

    feat(js): add answer ex1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalProductPrice = 27000;
+let discount = 25;
+let paidAfterDiscount = (totalProductPrice * (100 - discount)) / 100;
+
+// console.log(paidAfterDiscount)
